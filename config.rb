3a5087a90797a951959ab4f11f165b079c1a8bad
Niranjan Paranjape <niranjangp@gmail.com>
2015-05-06 13:48:34 +0530

    Initial commit

---
+++ config.rb
@@ -0,0 +1,28 @@
+set :css_dir, 'css'
+
+set :js_dir, 'js'
+
+set :images_dir, 'img'
+
+# Build-specific configuration
+configure :build do
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method = :git
+  deploy.branch = "master"
+end
